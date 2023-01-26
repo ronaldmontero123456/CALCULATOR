a333722e210ee52a3bc1838b5a51e65f3d8dad45
ronald <rmontero@gmail.com>
2023-01-25 21:54:14 -0400

    PutCal

---
+++ CALCULATOR.API/Controllers/CalculateController.cs
@@ -0,0 +1,36 @@
+﻿using CALCULATOR.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace CALCULATOR.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculateController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GetResultOfCalculate(string value)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                var response = dt.Compute(value, "");
+
+                var result = new WebResponse<double>()
+                {
+                    Body = (double)response,
+                    IsSuccess = true,
+                    Message = "Success",
+                    StatusCode = "200",
+                };
+
+                return Ok(result);
+            }
+            catch(Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ CALCULATOR.API/Data/WebResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CALCULATOR.API.Data
+{
+    public class WebResponse<T>
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public string StatusCode { get; set; }
+        public T Body { get; set; }
+    }
+}
